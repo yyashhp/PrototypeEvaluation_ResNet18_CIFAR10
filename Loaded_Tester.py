121642275b434a3bdec3629314ccb5d4d609d5e2
yyashhp <yxp441@miami.edu>
2023-06-23 12:47:20 -0400

    fixing metrics

--- Loaded_Tester.py
+++ Loaded_Tester.py
@@ -460,7 +460,7 @@ def main():
            \t cumulative alpha \t Cumuluative cs diff\
             \t Cumulative L2 Diff  \n")
         for i in range(len(data_schedule)):
-            f.write(f"{data_schedule[i]} \n {final_comb_alphas_avg[i]}\n {[val.item for val in final_ind_cs_diffs[i]]}\n {final_ind_l2_diffs[i].tolist()}\n {final_comb_cum_alphas_avg[i]}\t {final_comb_cs_diffs[i]}\t\\"
+            f.write(f"{data_schedule[i]} \n {final_comb_alphas_avg[i]}\n {[val.item() for val in final_ind_cs_diffs[i]]}\n {final_ind_l2_diffs[i].tolist()}\n {final_comb_cum_alphas_avg[i]}\t {final_comb_cs_diffs[i]}\t\\"
                     f" {final_comb_l2_diffs[i]}\t \
                 \n")
     f.close()
