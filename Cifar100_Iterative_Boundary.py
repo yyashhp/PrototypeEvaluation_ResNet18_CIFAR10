3c84f0cae779f78675d8e4f666f2a91aba1f20b8
yyashhp <yxp441@miami.edu>
2023-07-17 15:27:24 -0400

    fixing metrics

--- Cifar100_Iterative_Boundary.py
+++ Cifar100_Iterative_Boundary.py
@@ -72,7 +72,7 @@ print ("cuda: ", use_cuda)
 
 MEAN = [0.5] * 3
 STD = [0.5] * 3
-data_schedule = [0.7]
+data_schedule = [0.8]
 train_transform = transforms.Compose([transforms.ToTensor(),
                                       transforms.RandomHorizontalFlip(),
                                       transforms.RandomCrop(32, padding=4)])
@@ -186,7 +186,7 @@ def main():
     preds_matrix = torch.zeros(nclass, nclass, dtype=torch.float)
     for j in range(len(data_schedule)):
         model = ResNet18(nclass=nclass, scale=args.model_scale, channels=nchannels, **kwargsUser).to(device)
-        model_saved = torch.load(f"{saved_model_path}/{j+3}_Saved_Model_with_{data_schedule[j]}_CIFAR100_Data_0621_13_24_49", map_location=device)
+        model_saved = torch.load(f"{saved_model_path}/{j+4}_Saved_Model_with_{data_schedule[j]}_CIFAR100_Data_0621_13_24_49", map_location=device)
         model.load_state_dict(model_saved)
         for p in model.parameters():
             p.requires_grad = False
@@ -501,7 +501,7 @@ def main():
                             mispredictions.append([set, k, i, preds.item()])
                         #if i == 6:
                         preds_matrix[i][k] = preds
-                        with open('{}/Iterative_CIFAR100_split4_Until_Low_Loss_BOUNDARY_PROBS_{}.txt'.format(model_dir, date_time),
+                        with open('{}/Iterative_CIFAR100_split5_Until_Low_Loss_BOUNDARY_PROBS_{}.txt'.format(model_dir, date_time),
                                   'a') as f:
                             f.write(
                                 f"Going from {k} to {i}, batch {t},\t Iterations Needed: {iterations}\n\n")
@@ -533,8 +533,8 @@ def main():
 
           #  stacked_trained_l2.append(torch.stack(batch_l2_trained_diff, dim=0))
 
-            torch.save(torch.stack(set_latent_boundaries, dim = 0), f"{saved_boundaries_path}/Final4_{data_schedule[j]}7_Boundaries_Latents_{date_time}.pt")
-            torch.save(torch.stack(set_trained_boundaries, dim=0), f"{saved_boundaries_path}/Final4_{data_schedule[j]}7_Boundaries_Images_{date_time}.pt")
+            torch.save(torch.stack(set_latent_boundaries, dim = 0), f"{saved_boundaries_path}/Final5_{data_schedule[j]}7_Boundaries_Latents_{date_time}.pt")
+            torch.save(torch.stack(set_trained_boundaries, dim=0), f"{saved_boundaries_path}/Final5_{data_schedule[j]}7_Boundaries_Images_{date_time}.pt")
 
             stacked_sets_latent_boundaries.append(torch.stack(set_latent_boundaries, dim=0))
       # combined_boundary_images = torch.mean(torch.stack(stacked_sets_trained_boundaries,dim=0), dim=0)
@@ -691,7 +691,7 @@ def main():
         torch.save(stacked_sets_trained_boundaries[j],
                    f"{saved_boundaries_path}/Final_{data_schedule[j]}_Boundaries_TrainedIms_{date_time}.pt")
 
-    with open('{}/Iterative_CIFAR100_split4_Until_Low_Loss_BOUNDARY_{}.txt'.format(model_dir, date_time), 'a') as f:
+    with open('{}/Iterative_CIFAR100_split5_Until_Low_Loss_BOUNDARY_{}.txt'.format(model_dir, date_time), 'a') as f:
         #for i in range(6, len(data_schedule)):
         # f.write(f"\n Split: {data_schedule[i]} \t Alphas: {final_comb_alphas_avg[i]}  \t cumulative alpha: {final_comb_cum_alphas_avg[i]} \t CS_Line_Diffs:\
             #  {[val.item() for val in final_ind_cs_diffs[i]]} \
@@ -733,7 +733,7 @@ def main():
         plt.plot(data_schedule, final_comb_trained_col_cs_std[t], label="column-wise std")
         plt.plot(data_schedule, final_comb_trained_cs_std[t], label="row-wise std")
         plt.legend()
-        plt.savefig(f"{model_dir}/../PrototypeEvaluation_ResNet18_CIFAR10/metric_plots/{date_time}_CIFAR100_split4Batch{t}.png")
+        plt.savefig(f"{model_dir}/../PrototypeEvaluation_ResNet18_CIFAR10/metric_plots/{date_time}_CIFAR100_split5Batch{t}.png")
         plt.show()
         plt.figure().clear()
         plt.close()
