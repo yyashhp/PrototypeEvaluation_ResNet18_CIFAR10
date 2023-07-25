b2ce57720804f2c5283bf9dfd84f37c8897de47f
Yash patel <113195427+yyashhp@users.noreply.github.com>
2023-07-25 14:50:00 -0400

    Batch 4 boundaries

--- Cif10BDData.py
+++ Cif10BDData.py
@@ -945,7 +945,7 @@ def main():
     overall_col_high_outliers = torch.mean(torch.Tensor(col_high_outliers_total).clone(), dim=0)
     overall_row_low_outliers = torch.mean(torch.Tensor(row_low_outliers_total).clone(), dim=0)
     overall_col_low_outliers = torch.mean(torch.Tensor(col_low_outliers_total).clone(), dim=0)
-    print(overall_row_low_outliers)
+    print(overall_row_low_outliers.shape())
     with open('{}/outlier_data{}.txt'.format(model_dir, date_time), 'a') as f:
         for i in range(len(data_schedule)):
             f.write(f"Data Split : {data_schedule[i]} \t Row Mean: {overall_interrow_cs_diffs[i]} \t Row_Std: {overall_interrow_cs_stds[i]} \
